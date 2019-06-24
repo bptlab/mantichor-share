ea38080f21f91952e88acc89155fe0f051658d55
Christian Friedow <christian@friedow.com>
2019-06-24 17:18:32 +0200

    Added get route

--- src/routes/choreographies.ts
+++ src/routes/choreographies.ts
@@ -3,10 +3,16 @@ import Choreography from '@/models/Choreography';
 
 const router: express.Router = express.Router();
 
+router.get('/:id', async (req: express.Request, res: express.Response) => {
+
+    const choreography = await Choreography.findById(req.params.id).exec();
+    res.send(JSON.stringify(choreography));
+});
+
 router.post('/', async (req: express.Request, res: express.Response) => {
   const newChoreography = new Choreography(req.body);
   await newChoreography.save();
-  res.status(201).send(newChoreography._id);
+  res.status(201).send(`${newChoreography._id}`);
 });
 
 export default router;
